c1b5ff0ba07cdc930dcc99812af977b935a65c68
sanjayatrital1 <snjtrital@gmail.com>
2019-08-10 20:54:43 -0500

    product service added

--- onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/service/impl/ProductServiceImpl.java
+++ onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/service/impl/ProductServiceImpl.java
@@ -1,7 +1,9 @@
 package edu.mum.cs.onlinemarketplace.service.impl;
 
 import edu.mum.cs.onlinemarketplace.domain.Product;
+import edu.mum.cs.onlinemarketplace.repository.ProductRepository;
 import edu.mum.cs.onlinemarketplace.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,13 +12,17 @@ import java.util.List;
 @Service
 @Transactional
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public List<Product> getAllProducts() {
-        return null;
+        return productRepository.findAll();
     }
 
     @Override
     public Product save(Product product) {
-        return null;
+        return productRepository.save(product);
     }
 }
