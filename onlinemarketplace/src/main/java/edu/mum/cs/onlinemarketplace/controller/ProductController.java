ef88f7b4517c92569383dd0465d0a08eef07904d
sanjayatrital1 <snjtrital@gmail.com>
2019-08-10 21:20:13 -0500

    product controller added

--- onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/controller/ProductController.java
+++ onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/controller/ProductController.java
@@ -1,17 +1,34 @@
 package edu.mum.cs.onlinemarketplace.controller;
 
+import edu.mum.cs.onlinemarketplace.domain.Product;
 import edu.mum.cs.onlinemarketplace.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/product")
-    public String getProductFzorm(){
+    @GetMapping("/product/{uid}")
+    public String getProductFzorm(@PathVariable Long uid, @ModelAttribute Product product){
         return "productForm";
+    }
+
+    @PostMapping("/product/{uid}")
+    public String saveProduct(Product product, BindingResult bindingResult, Model model ){
+        if(bindingResult.hasErrors()){
+            return "productForm";
+        }
+        else {
+            productService.save(product);
+            return "redirect;/showDetais";
+        }
     }
 }
