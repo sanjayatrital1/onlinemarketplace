b16ffb4a573769ee76634cbbcb03b5d9416a8aa7
sanjayatrital1 <snjtrital@gmail.com>
2019-08-10 22:14:58 -0500

    add product page added

--- onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/controller/ProductController.java
+++ onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/controller/ProductController.java
@@ -5,11 +5,17 @@ import edu.mum.cs.onlinemarketplace.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.io.File;
 
 @Controller
 public class ProductController {
@@ -17,18 +23,37 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/product/{uid}")
-    public String getProductFzorm(@PathVariable Long uid, @ModelAttribute Product product){
-        return "productForm";
+    public String getProductForm(@PathVariable Long uid, @ModelAttribute Product product){
+        return "addProductForm";
     }
 
     @PostMapping("/product/{uid}")
-    public String saveProduct(Product product, BindingResult bindingResult, Model model ){
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model, HttpServletRequest request ){
         if(bindingResult.hasErrors()){
-            return "productForm";
+            return "addProductForm";
         }
         else {
+
+            String[] suppressedFields = bindingResult.getSuppressedFields();
+            if (suppressedFields.length > 0) {
+                throw new RuntimeException("Attempting to bind disallowed fields: "
+                        + StringUtils.arrayToCommaDelimitedString(suppressedFields));
+            }
+
+            MultipartFile productImage = product.getProductImage();
+            String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+            if (productImage != null && !productImage.isEmpty()) {
+                try {
+                    System.out.println(rootDirectory + "resources\\statis\\images\\" + product.getId() + ".png");
+                    productImage.transferTo(
+                            new File(rootDirectory + "resources\\static\\images\\" + product.getId() + ".png"));
+                } catch (Exception e) {
+                    throw new RuntimeException("Product Image saving failed", e);
+                }
+            }
+
             productService.save(product);
-            return "redirect;/showDetais";
+            return "redirect:/showProductDetais";
         }
     }
 }
