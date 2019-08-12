7af12fae5a527d3720d3b5da9338caafd3888c9b
sanjayatrital1 <snjtrital@gmail.com>
2019-08-11 23:44:36 -0500

    push

--- onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/repository/ProductRepository.java
+++ onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/repository/ProductRepository.java
@@ -12,4 +12,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product,Long> {
     @Query("select p from Product p where p.seller.id=:id")
     public List<Product>getProductsBySellerId(Long id);
+
+    @Query("delete from Product p where p.id=:id")
+    public void deleteById(Long id);
 }
