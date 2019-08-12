df338ed66327ee2beb85e073bf098e049978fe43
Diego Kramer <diego.kramer99@gmail.com>
2019-08-11 23:17:41 -0500

    Data.sql ok

--- onlinemarketplace/src/main/resources/data.sql
+++ onlinemarketplace/src/main/resources/data.sql
@@ -1,12 +1,11 @@
 
-INSERT INTO `ADDRESS` (id, city, state, status, street, zip_code) VALUES (1, 'Fairfield', 'IA', 'Shipping', '1000 North 4th', '52557');
-INSERT INTO `ADDRESS` (id, city, state, status, street, zip_code) VALUES (2, 'Fairfield-Shipping', 'IA', 'Shipping', '9874 North 10th', '52557');
-INSERT INTO `ADDRESS` (id, city, state, status, street, zip_code) VALUES (3, 'Fairfield', 'IA', 'Shipping', '1000 North 4th', '52557');
+INSERT INTO `USER` (id, name, email) VALUES (1, 'First User', 'first.user@gmail.com');
+INSERT INTO `USER` (id, name, email) VALUES (2, 'Second User', 'second.user@gmail.com');
+INSERT INTO `USER` (id, name, email) VALUES (3, 'Third User', 'third.user@gmail.com');
 
-INSERT INTO `USER` (id, name, email, billing_address_id, shipping_address_id, type) VALUES (1, 'First User', 'first.user@gmail.com', 1, 2, 'SELLER');
-INSERT INTO `USER` (id, name, email, billing_address_id, shipping_address_id, type) VALUES (2, 'Second User', 'second.user@gmail.com', 1, 2, 'SELLER');
-INSERT INTO `USER` (id, name, email, billing_address_id, shipping_address_id, type) VALUES (3, 'Third User', 'third.user@gmail.com', 1, 2, 'SELLER');
-
+INSERT INTO `ADDRESS` (id, city, state, status, street, zip_code, user_id) VALUES (1, 'Fairfield', 'IA', 'Shipping', '1000 North 4th', '52557', 1);
+INSERT INTO `ADDRESS` (id, city, state, status, street, zip_code, user_id) VALUES (2, 'Fairfield', 'IA', 'Shipping', '1000 North 4th', '52557', 2);
+INSERT INTO `ADDRESS` (id, city, state, status, street, zip_code, user_id) VALUES (3, 'Fairfield', 'IA', 'Shipping', '1000 North 4th', '52557', 3);
 
 INSERT INTO `PRODUCT` (id, name, description, price, seller_id, quantiy) VALUES (1, 'Product01', 'Product01 description', 10, 1, 2);
 INSERT INTO `PRODUCT` (id, name, description, price, seller_id, quantiy) VALUES (2, 'Product02', 'Product02 description', 10, 1, 3);
@@ -20,18 +19,28 @@ INSERT INTO `PRODUCT` (id, name, description, price, seller_id, quantiy) VALUES 
 
 
 INSERT INTO `CART` (id, total_price, buyer_id, active) VALUES (1, 30, 2, true);
-INSERT INTO `CART` (id, total_price, buyer_id, active) VALUES (2, 30, null, false);
+INSERT INTO `CART` (id, total_price, buyer_id, active) VALUES (2, 50, 2, false);
 
-INSERT INTO `CART_PRODUCT_LIST` (cart_id, product_list_id) VALUES (1, 1);
-INSERT INTO `CART_PRODUCT_LIST` (cart_id, product_list_id) VALUES (1, 2);
-INSERT INTO `CART_PRODUCT_LIST` (cart_id, product_list_id) VALUES (1, 7);
-INSERT INTO `CART_PRODUCT_LIST` (cart_id, product_list_id) VALUES (1, 8);
+INSERT INTO `USER_ORDER` (id, status, cart_id, seller_id, create_date, total) VALUES (1, 'waiting', 1, 1, '2019-08-10', 20);
+INSERT INTO `USER_ORDER` (id, status, cart_id, seller_id, create_date, total) VALUES (2, 'waiting', 1, 3, '2019-08-10', 20);
 
-INSERT INTO `CART_PRODUCT_LIST` (cart_id, product_list_id) VALUES (2, 3);
-INSERT INTO `CART_PRODUCT_LIST` (cart_id, product_list_id) VALUES (2, 4);
+INSERT INTO `USER` (id, name, email,status,type) VALUES (1, 'First User', 'first.user@gmail.com',null,'SELLER');
+INSERT INTO `USER` (id, name, email,status,type) VALUES (2, 'Second User', 'second.user@gmail.com',null,'SELLER');
+
+INSERT INTO `ADDRESS` (id, city, state, status, street, zip_code,user_id) VALUES (1, 'Fairfield', 'IA', 'Shipping', '1000 North 4th', '52557',1);
+INSERT INTO `ADDRESS` (id, city, state, status, street, zip_code,user_id) VALUES (2, 'Fairfield', 'IA', 'Shipping', '1000 North 4th', '52557',2);
 
 
--- INSERT INTO `USER_ORDER` (id, status, cart_id, seller_id, create_date, total) VALUES (1, 'waiting', 1, 1, '2019-08-10', 20);
--- INSERT INTO `USER_ORDER` (id, status, cart_id, seller_id, create_date, total) VALUES (2, 'waiting', 1, 3, '2019-08-10', 20);
+INSERT INTO `PRODUCT` (id, name, description, price,  quantiy) VALUES (1, 'Product01', 'Product01 description', 10,  2);
+INSERT INTO `PRODUCT` (id, name, description, price, quantiy) VALUES (2, 'Product02', 'Product02 description', 10,  3);
+INSERT INTO `PRODUCT` (id, name, description, price, quantiy) VALUES (3, 'Product03', 'Product03 description', 10,  2);
+INSERT INTO `PRODUCT` (id, name, description, price,  quantiy) VALUES (4, 'Product04', 'Product04 description', 10,  3);
 
+
+
+INSERT INTO `CART` (id, total_price) VALUES (1, 30);
+INSERT INTO `CART` (id, total_price) VALUES (2, 50);
+
+
+INSERT INTO `USER_ORDER` (id, status, create_date) VALUES (1, 'waiting', '2019-08-10');
 
